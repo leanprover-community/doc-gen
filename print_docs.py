635bde608f4134288fc788a66c891d9486f8e9ae
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-09-01 10:25:33 -0400

    Update print_docs.py

--- print_docs.py
+++ print_docs.py
@@ -397,7 +397,7 @@ def copy_css(path, use_symlinks):
 
 def copy_yaml_files(path):
   for fn in ['100.yaml', 'undergrad.yaml', 'overview.yaml']:
-    shutil.copyfile(f'mathlib/docs/{fn}', path+fn)
+    shutil.copyfile(f'{local_lean_root}docs/{fn}', path+fn)
 
 def write_decl_txt(loc_map):
   with open_outfile('decl.txt') as out:
