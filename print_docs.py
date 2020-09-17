ac38483d56a3315dbcc6ddab39291754a96ac8ee
Alex Peattie <alexpeattie@gmail.com>
2020-09-17 18:58:01 +0100

    Title tag fix

--- print_docs.py
+++ print_docs.py
@@ -230,10 +230,8 @@ def kind_of_decl(decl):
   return kind
 env.globals['kind_of_decl'] = kind_of_decl
 
-# Inserts zero-width spaces after dots
 def htmlify_name(n):
-  # TODO: html escape
-  return '.'.join([f'<span>{ part }</span>' for part in n.split('.')])
+  return '.'.join([f'<span>{ html.escape(part) }</span>' for part in n.split('.')])
 env.filters['htmlify_name'] = htmlify_name
 
 # returns (pagetitle, intro_block), [(tactic_name, tactic_block)]
